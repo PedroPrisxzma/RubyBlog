ef7ec9b440cee82699867613d7cbe0b69f3c737a
Pedro <pedro.costa@rakuten.com>
2024-11-14 17:16:59 +0900

    Added trix, action_text content usage and a workaround for an error in the edit part, when the rich_text_are was not displaying correctly

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -1,22 +1,24 @@
 class BlogPost < ApplicationRecord
-    validates :title, presence: true
-    validates :body, presence: true
+  has_rich_text :content
 
-    scope :sorted, -> { order(arel_table[:published_at].desc.nulls_last).order(updated_at: :desc)}
-    scope :draft, -> { where(published_at: nil) }
-    scope :published, -> { where("published_at <= ?", Time.current) }
-    scope :scheduled, -> { where("published_at > ?", Time.current) }
+  validates :title, presence: true
+  validates :content, presence: true
 
-    def draft?
-      published_at.nil?
-    end
+  scope :sorted, -> { order(arel_table[:published_at].desc.nulls_last).order(updated_at: :desc)}
+  scope :draft, -> { where(published_at: nil) }
+  scope :published, -> { where("published_at <= ?", Time.current) }
+  scope :scheduled, -> { where("published_at > ?", Time.current) }
 
-    def published?
-      published_at? && published_at <= Time.current
-    end
+  def draft?
+    published_at.nil?
+  end
 
-    def scheduled?
-      published_at? && published_at > Time.current
-    end
+  def published?
+    published_at? && published_at <= Time.current
+  end
+
+  def scheduled?
+    published_at? && published_at > Time.current
+  end
 end
 
