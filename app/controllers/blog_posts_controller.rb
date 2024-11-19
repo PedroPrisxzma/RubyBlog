9bc4a84697d48766ab47c0d6f22539bdf2f1dae9
Pedro <pedro.costa@rakuten.com>
2024-11-19 17:20:14 +0900

    Added Pagy and using pagination. Also added some dummy blogposts to see the pages in action

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -3,6 +3,7 @@ class BlogPostsController < ApplicationController
   before_action :set_blog_post, except: [:index, :new, :create] #only: [:edit, :update, :show, :destory]
   def index
     @blog_posts = user_signed_in? ? BlogPost.sorted : BlogPost.published.sorted
+    @pagy, @blog_posts = pagy(@blog_posts)
   end
   def show
   rescue ActiveRecord::RecordNotFound
